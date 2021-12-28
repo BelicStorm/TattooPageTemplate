bf17fe784feeb36fc7eb63e2e22feeafb4141924
Cristian Pardo Casanova <49480754+BelicStorm@users.noreply.github.com>
2021-12-28 19:50:17 +0100

    add artists page

---
+++ pages/artistas.js
@@ -0,0 +1,9 @@
+import Layout from "../components/layout.component";
+
+export default function ArtistsPage() {
+  return (
+    <Layout actual="artistas">
+      <section className="ArtContainer container"></section>
+    </Layout>
+  );
+}
