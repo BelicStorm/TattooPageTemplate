7d693e7bbe938c2a2d58e605313968d039023c02
Cristian Pardo <cpardoca3@gmail.com>
2022-02-02 12:39:07 +0100

    Forms

--- components/arcodion.component.js
+++ components/arcodion.component.js
@@ -1,10 +1,9 @@
 export const Acordeon = ({ childs }) => {
-    console.log(childs);
     return (
         <div className="acordeon-tabs">
             {
                 childs.map((acordeonContent, index)=>{
-                    return <div className="acordeon-tab" key={Math.random()}>
+                    return <div className="acordeon-tab" key={acordeonContent.title}>
                         <input type="checkbox" className="acordeon-checkbox" id={index}/>
                         <label className="acordeon-tab-label" htmlFor={index}>{acordeonContent.title}</label>
                         <div className="acordeon-tab-content">
--- pages/contacto.js
+++ pages/contacto.js
@@ -1,135 +1,183 @@
-import Link from "next/dist/client/link";
-import { useState } from 'react'
 import { Acordeon } from "../components/arcodion.component";
 import Layout from "../components/layout.component";
 import { Title } from "../components/title.component";
-/* https://medium.com/nerd-for-tech/coding-a-contact-form-with-next-js-and-nodemailer-d3a8dc6cd645 */
+import { ValidationError, useForm } from '@formspree/react';
+import Image from 'next/image'
 
 const SimpleContactForm = () => {
-    const [name, setName] = useState('')
-    const [email, setEmail] = useState('')
-    const [message, setMessage] = useState('')
-    const [submitted, setSubmitted] = useState(false)
-    const handleSubmit = (e) => { 
-        e.preventDefault()
-        console.log('Sending')
-      let data = {
-          name,
-          email,
-          message
-        }
-        fetch('/api/contact', {
-            method: 'POST',
-            headers: {
-              'Accept': 'application/json, text/plain, */*',
-              'Content-Type': 'application/json'
-            },
-            body: JSON.stringify(data)
-          }).then((res) => {
-            console.log('Response received')
-            if (res.status === 200) {
-              console.log('Response succeeded!')
-              setSubmitted(true)
-              setName('')
-              setEmail('')
-              setBody('')
-            }
-        })
+    /* crear cuenta en formspree y usar el id */
+    const [state, handleSubmit] = useForm('mgerqenw', {
+        data: {
+          pageTitle: 'Mensaje desde de prueba',
+    }});
+    if (state.succeeded) {
+        return <div>¡Nos pondremos en contacto pronto! Revisa tu correo o estate atento a tu teléfono.</div>;
     }
-    return  <form action='' className='form'>
+    return  <form className='form' onSubmit={handleSubmit}>
                 <p className='field required'>
-                    <label className='label required' for='name'>Full name</label>
+                    <label className='label required' htmlFor='name'>Nombre</label>
                     <input className='text-input' id='name' name='name' required type='text'/>
+                    <ValidationError field="name" prefix="name" errors={state.errors} />
                 </p>
                 <p className='field required half'>
-                    <label className='label' for='email'>E-mail</label>
+                    <label className='label' htmlFor='email'>E-mail</label>
                     <input className='text-input' id='email' name='email' required type='email'/>
+                    <ValidationError field="name" prefix="name" errors={state.errors} />
                 </p>
                 <p className='field half'>
-                    <label className='label' for='phone'>Phone</label>
+                    <label className='label' htmlFor='phone'>Teléfono</label>
                     <input className='text-input' id='phone' name='phone' type='phone'/>
+                    <ValidationError field="phone" prefix="phone" errors={state.errors} />
                 </p>
-                <p className='field half required error'>
-                    <label className='label' for='login'>Login</label>
-                    <input className='text-input' id='login' name='login' required type='text'/>
-                </p>
-                <p className='field half required'>
-                    <label className='label' for='password'>Password</label>
-                    <input className='text-input' id='password' name='password' required type='password'/>
-                </p>
-                <div className='field'>
-                    <label className='label'>Sport?</label>
-                    <ul className='checkboxes'>
-                        <li className='checkbox'>
-                        <input className='checkbox-input' id='choice-0' name='choice' type='checkbox' value='0'/>
-                        <label className='checkbox-label' for='choice-0'>Football</label>
-                        </li>
-                        <li className='checkbox'>
-                        <input className='checkbox-input' id='choice-1' name='choice' type='checkbox' value='1'/>
-                        <label className='checkbox-label' for='choice-1'>Basketball</label>
-                        </li>
-                        <li className='checkbox'>
-                        <input className='checkbox-input' id='choice-2' name='choice' type='checkbox' value='2'/>
-                        <label className='checkbox-label' for='choice-2'>Volleyball</label>
-                        </li>
-                        <li className='checkbox'>
-                        <input className='checkbox-input' id='choice-3' name='choice' type='checkbox' value='3'/>
-                        <label className='checkbox-label' for='choice-3'>Golf</label>
-                        </li>
-                        <li className='checkbox'>
-                        <input className='checkbox-input' id='choice-4' name='choice' type='checkbox' value='4'/>
-                        <label className='checkbox-label' for='choice-4'>Swimming</label>
-                        </li>
-                    </ul>
-                </div>
-                <div className='field'>
-                    <label className='label'>Favourite JS framework</label>
-                    <ul className='options'>
-                        <li className='option'>
-                        <input className='option-input' id='option-0' name='option' type='radio' value='0'/>
-                        <label className='option-label' for='option-0'>React</label>
-                        </li>
-                        <li className='option'>
-                        <input className='option-input' id='option-1' name='option' type='radio' value='1'/>
-                        <label className='option-label' for='option-1'>Vue</label>
-                        </li>
-                        <li className='option'>
-                        <input className='option-input' id='option-2' name='option' type='radio' value='2'/>
-                        <label className='option-label' for='option-2'>Angular</label>
-                        </li>
-                        <li className='option'>
-                        <input className='option-input' id='option-3' name='option' type='radio' value='3'/>
-                        <label className='option-label' for='option-3'>Riot</label>
-                        </li>
-                        <li className='option'>
-                        <input className='option-input' id='option-4' name='option' type='radio' value='4'/>
-                        <label className='option-label' for='option-4'>Polymer</label>
-                        </li>
-                        <li className='option'>
-                        <input className='option-input' id='option-5' name='option' type='radio' value='5'/>
-                        <label className='option-label' for='option-5'>Ember</label>
-                        </li>
-                        <li className='option'>
-                        <input className='option-input' id='option-6' name='option' type='radio' value='6'/>
-                        <label className='option-label' for='option-6'>Meteor</label>
-                        </li>
-                    </ul>
-                </div>
+
+
                 <p className='field'>
-                    <label className='label' for='about'>About</label>
+                    <label className='label' htmlFor='about'>Mensaje</label>
                     <textarea className='textarea' cols='50' id='about' name='about' rows='4'></textarea>
+                    <ValidationError field="about" prefix="about" errors={state.errors} />
+                    
+                </p>
+
+                <p className='field'>
+                    <input className='button' type='submit' value='Enviar'/>
+                </p>
+            </form>
+}
+const TattooContact = () => {
+    /* crear cuenta en formspree y usar el id */
+    const [state, handleSubmit] = useForm('mgerqenw', {
+        data: {
+          pageTitle: 'Mensaje desde de prueba para Tattoo',
+    }});
+    if (state.succeeded) {
+        return <div>¡Nos pondremos en contacto pronto! Revisa tu correo o estate atento a tu teléfono.</div>;
+    }
+    return  <form  onSubmit={handleSubmit} className='form'>
+                <p className='field required'>
+                    <label className='label required' htmlFor='name'>Full name</label>
+                    <input className='text-input' id='name' name='name' required type='text'/>
+                    <ValidationError field="name" prefix="name" errors={state.errors} />
+                </p>
+                <p className='field required half'>
+                    <label className='label' htmlFor='email'>E-mail</label>
+                    <input className='text-input' id='email' name='email' required type='email'/>
+                    <ValidationError field="email" prefix="email" errors={state.errors} />
                 </p>
                 <p className='field half'>
-                    <label className='label' for='select'>Position</label>
+                    <label className='label' htmlFor='phone'>Phone</label>
+                    <input className='text-input' id='phone' name='phone' type='phone' required/>
+                    <ValidationError field="phone" prefix="phone" errors={state.errors} />
+                </p>
+                <p className='field half'>
+                    <label className='label' htmlFor='Artist'>Tattoo Artist</label>
                     <select className='select' id='select'>
-                        <option selected value=''></option>
-                        <option value='ceo'>CEO</option>
-                        <option value='front-end'>Front-end developer</option>
-                        <option value='back-end'>Back-end developer</option>
+                        <option defaultValue='Sin preferencia'>Sin preferencia</option>
+                        <option value='Gabo'>Gabo</option>
+                        <option value='Alba'>Alba Ferrero</option>
+                        <option value='etc'>etc</option>
                     </select>
+                    <ValidationError field="Artist" prefix="Artist" errors={state.errors} />
                 </p>
                 <p className='field half'>
-                    <input className='button' type='submit' value='Send'/>
+                    <label className='label' htmlFor='Tamaño'>Tamaño</label>
+                    <select className='select' id='select'>
+                        <option defaultValue='Sin preferencia'>Sin preferencia</option>
+                        <option value='1-5cm'>1-5cm</option>
+                        <option value='6-10cm'>6-10cm</option>
+                        <option value='11-20cm'>11-20cm</option>
+                        <option value='+20cm'>+20cm</option>
+                    </select>
+                    <ValidationError field="Tamaño" prefix="Tamaño" errors={state.errors} />
+                </p>
+                <p className='field half'>
+                    <label className='label' htmlFor='edad'>Edad</label>
+                    <input className='text-input' id='edad' name='edad' type='number' min="1" max="99" required/>
+                    <ValidationError field="edad" prefix="edad" errors={state.errors} />
+                </p>
+                <p className='field half'>
+                    <label className='label' htmlFor='Referencia'>Referencia</label>
+                    <input type="file" name="Referencia" accept="image/png, image/jpeg"/>
+                    <ValidationError field="Referencia" prefix="Referencia" errors={state.errors} />
+                </p>
+               
+                <div className="formImageContainer">
+                        <Image
+                            src={"/media/Pain-Chart.jpg"}
+                            alt="Grafica de dolor"
+                            width={500}
+                            height={300}
+                        />
+                </div>
+                
+                <p className='field'>
+                    <label className='label' htmlFor='Explicacion'>¿Qué quieres y donde?</label>
+                    <textarea className='textarea' cols='50' id='Explicacion' name='Explicacion' rows='4' required 
+                              placeholder='Explica el tattoo que quieres hacerte, si es a color, o blanco y negro, el estilo y donde lo quieres.'></textarea>
+                    <ValidationError field="Explicacion" prefix="Explicacion" errors={state.errors} />
+                </p>
+                <p className='field'>
+                    <label className='label' htmlFor='Disponibilidad'>Disponibilidad</label>
+                    <textarea className='textarea' cols='50' id='Disponibilidad' name='Disponibilidad' rows='4' required 
+                              placeholder='Explicanos que dias de la semana o a que horas puedes pasarte.'></textarea>
+                    <ValidationError field="Disponibilidad" prefix="Disponibilidad" errors={state.errors} />
+                </p>
+               
+                <p className='field'>
+                    <input className='button' type='submit' value='Enviar'/>
+                </p>
+            </form>
+}
+const PiercingContact = () => {
+    /* crear cuenta en formspree y usar el id */
+    const [state, handleSubmit] = useForm('mgerqenw', {
+        data: {
+          pageTitle: 'Mensaje desde de prueba Piercing',
+    }});
+    if (state.succeeded) {
+        return <div>¡Nos pondremos en contacto pronto! Revisa tu correo o estate atento a tu teléfono.</div>;
+    }
+   return  <form  onSubmit={handleSubmit} className='form'>
+                <p className='field required'>
+                    <label className='label required' htmlFor='name'>Full name</label>
+                    <input className='text-input' id='name' name='name' required type='text'/>
+                    <ValidationError field="name" prefix="name" errors={state.errors} />
+                </p>
+                <p className='field required half'>
+                    <label className='label' htmlFor='email'>E-mail</label>
+                    <input className='text-input' id='email' name='email' required type='email'/>
+                    <ValidationError field="email" prefix="email" errors={state.errors} />
+                </p>
+                <p className='field half'>
+                    <label className='label' htmlFor='phone'>Phone</label>
+                    <input className='text-input' id='phone' name='phone' type='phone' required/>
+                    <ValidationError field="phone" prefix="phone" errors={state.errors} />
+                </p>
+                <p className='field half'>
+                    <label className='label' htmlFor='Artist'>Parte del cuerpo</label>
+                    <select className='select' id='select'>
+                        <option defaultValue='Sin preferencia'>Sin preferencia</option>
+                        <option value='Oreja'>Oreja</option>
+                        <option value='Boca'>Boca</option>
+                        <option value='etc'>etc</option>
+                    </select>
+                    <ValidationError field="Artist" prefix="Artist" errors={state.errors} />
+                </p>
+
+                <p className='field half'>
+                    <label className='label' htmlFor='edad'>Edad</label>
+                    <input className='text-input' id='edad' name='edad' type='number' min="1" max="99" required/>
+                    <ValidationError field="edad" prefix="edad" errors={state.errors} />
+                </p>
+
+                <p className='field'>
+                    <label className='label' htmlFor='Disponibilidad'>Disponibilidad</label>
+                    <textarea className='textarea' cols='50' id='Disponibilidad' name='Disponibilidad' rows='4' required 
+                              placeholder='Explicanos que dias de la semana o a que horas puedes pasarte.'></textarea>
+                    <ValidationError field="Disponibilidad" prefix="Disponibilidad" errors={state.errors} />
+                </p>
+               
+                <p className='field'>
+                    <input className='button' type='submit' value='Enviar'/>
                 </p>
             </form>
 }
@@ -140,7 +188,9 @@ export default function ContactFormTest() {
            <div className="contactFormsContainer">
             <Title center="Contacta con nosotros" sub={"Te guiamos para que pidas cita"}></Title>
             <Acordeon childs={[
-                {title:"Contacto Test",child:<SimpleContactForm/>}
+                {title:"Ponte en contacto con nosotros",child:<SimpleContactForm/>},
+                {title:"Pide cita para un tattoo",child:<TattooContact/>},
+                {title:"Pide cita para un piercing",child:<PiercingContact/>}
             ]}></Acordeon>
            </div>
           </Layout>
