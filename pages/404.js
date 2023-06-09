ce1f092c44741d65525d970e1178ef06c9110276
Cristian Pardo Casanova <49480754+BelicStorm@users.noreply.github.com>
2023-06-09 17:18:56 +0200

    Espacio en 404.js

--- pages/404.js
+++ pages/404.js
@@ -2,7 +2,7 @@ import Link from "next/dist/client/link";
 export default function Custom404() {
     return <div className="gateToNothingness">
         <div className="ForgottenRealm">
-                <h1>404.</h1>
+                <h1>404. </h1>
                 <p>You have been lost in a kingdom forgotten by the hand of any god. Escape before it's too late. It is not a joke. In this place only eternal pain awaits you.</p>
                 <div className="salvation">
                     <Link
